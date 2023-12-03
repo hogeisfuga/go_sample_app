b6c2fd9144aff1827c8718e2aa84513c435b2984
Aki <120ce066@gmail.com>
2023-12-03 15:19:31 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module todo_app
+
+go 1.21.3
+
+require gopkg.in/go-ini/ini.v1 v1.67.0
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/stretchr/testify v1.8.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"todo_app/app/models"
+)
+
+func main() {
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("hey!")
+
+	fmt.Println(models.Db)
+
+}
